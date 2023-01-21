bc11b2a0cf3d1c0b2769e32d6988d4bc20a9b1d4
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-01-21 23:19:39 +0530

    changed the font-family to muli and reduced the size of the about component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import  Navbar from './components/navbar/Navbar';
 import  About from './components/about/About';
-
+import  Service from './components/services/Services';
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
      <About />
+     <Service />
     </div>
   );
 }
