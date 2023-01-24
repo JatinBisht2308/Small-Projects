b94c1af6ca528158826499f48a4a94bde7e0ff3c
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-01-24 11:40:29 +0530

    added the image

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Image1  from '../../assets/trafagar-about-image.png'
+import BackImg from '../../assets/about-element.png'
 import './about.css'
 const About = () => {
   return (
     <div className='about'>
+      <img src={BackImg} alt="" className='backimg-about' />
         <div className="left-about">
           <h1>Virtual Healthcare for you</h1>
           <p>Trafalgar provides progressive and affordable healthcare, accessible on mobile and online for everyone.</p>
