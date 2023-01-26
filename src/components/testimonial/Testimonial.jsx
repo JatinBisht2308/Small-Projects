3bb850ccdf46a6d76af084d16aeccb5008fee4a6
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-01-26 14:59:04 +0530

    :fire: created the testimonial card.jsx so that it is used again and again in the testimonial.jsx for different slider

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,32 +1,17 @@
 import React from "react";
 import "./testimonial.css";
-import Customer from "../../assets/testimonial.png";
+import Card from './testimonial_card';
+
 
 const Testimonial = () => {
   return (
     <div className="Testimonial">
-      <div className="testimonial_container">
-        <div className="testi_container_upper">
-          <h2 className="testi_heading">What our customer are saying</h2>
-          <div className="testi_underline"></div>
-        </div>
-        <div className="testi_container_bottom">
-          <div className="testi_left">
-            <img src={Customer} alt="" />
-            <div className="customer_info">
-              <small className="customer_name">Edward Newgate</small>
-              <small className="customer_company">Founder Circle</small>
-            </div>
-          </div>
-          <div className="testi_right">
-            <p className="customer_review">
-              “Our dedicated patient engagement app and web portal allow you to
-              access information instantaneously (no tedeous form, long calls,
-              or administrative hassle) and securely”
-            </p>
-          </div>
-        </div>
-      </div>
+      <Card/>
+      <Card/>
+      {/* <Card/>
+      <Card/>
+      <Card/>
+      <Card/> */}
     </div>
   );
 };
