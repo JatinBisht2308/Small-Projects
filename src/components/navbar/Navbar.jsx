b381590288c404ece379639d5da87f9f2e704b55
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-04-07 16:51:20 +0530

    :fire: downloaded the light mode and dark mode button

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './navbar.css'
 import Logo from '../../assets/logo.png'
+import darkMode from "../../assets/dark_mode.png";
+import lightMode from "../../assets/light_mode.png";
 const Navbar = () => {
   return (
     <div className="nav">
@@ -15,6 +17,7 @@ const Navbar = () => {
           <li className="nav-item four">Testimonial</li>
           <li className="nav-item five">About us</li>
         </ul>
+       <button className='mode_changer'></button>
       </div>
       </div>
   )
