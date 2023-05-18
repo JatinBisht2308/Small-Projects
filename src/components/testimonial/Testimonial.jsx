8279169c24b3bd407711cf79ce6a293160ff0eb8
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-05-19 03:24:30 +0530

    :fire: completed the testimonial carousel with custom data changing and animation effects

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,70 +1,82 @@
-import {React, useState} from "react";
+import { React, useState } from "react";
 import "./testimonial.css";
 import Card from "./testimonial_card";
 import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
+import "aos/dist/aos.css";
+import AOS from "aos";
 
 const Testimonial = () => {
-  const [isHighlight,setIsHighlight] = useState([false,true,false,false]);
-  const [dataIndex,setDataIndex] = useState(0);
-  const names = ["Edward Newgate","Tom Holland","Lionel Messi","Anish Thakur"];
-  const companies = ["Founder Circle","Marvel Studios","Barcelona","Pied Piper"];
+  const [isHighlight, setIsHighlight] = useState([false, true, false, false]);
+  const [dataIndex, setDataIndex] = useState(0);
+  const names = [
+    "Edward Newgate",
+    "Tom Holland",
+    "Lionel Messi",
+    "Anish Thakur",
+  ];
+  const companies = [
+    "Founder Circle",
+    "Marvel Studios",
+    "Barcelona",
+    "Pied Piper",
+  ];
   const feedbacks = [
-    "1)- Our dedicated patient engagement app and web portal allow you to access information instantaneously (no tedeous form, long calls, or administrative hassle) and securely",
-    "2)- Our dedicated patient engagement app and web portal allow you to access information instantaneously (no tedeous form, long calls, or administrative hassle) and securely",
-    "3)- Our dedicated patient engagement app and web portal allow you to access information instantaneously (no tedeous form, long calls, or administrative hassle) and securely",
-    "4)- Our dedicated patient engagement app and web portal allow you to access information instantaneously (no tedeous form, long calls, or administrative hassle) and securely"
-  ]
-  const leftTesti = () =>{
-    for(let i=0;i<isHighlight.length;i++){
-      if(isHighlight[i]===true)
-      {
-        if(i===0)
-        {
+    "Our dedicated patient engagement app and web portal allow you to access information instantaneously (no tedeous form, long calls, or administrative hassle) and securely",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus molestias magni quia dolore, sed expedita quidem? Iure modi vero reprehenderit laudantium nobis.",
+    "AOS allows you to animate elements as you scroll down, and up. If you scroll back to top, elements will animate to it's previous state and are ready to animate again",
+    "We choose because of the ability to adapt with feedback, ultimatley leading to the best experience for our members and team, Helped us a lot even in difficult situations! ",
+  ];
+  const leftTesti = () => {
+    for (let i = 0; i < isHighlight.length; i++) {
+      if (isHighlight[i] === true) {
+        if (i === 0) {
           setDataIndex(3);
-          setIsHighlight([false,false,false,true]);
-        }
-        else{
+          setIsHighlight([false, false, false, true]);
+        } else {
           isHighlight[i] = false;
-          isHighlight[i-1] = true;
-          setDataIndex(i-1);
+          isHighlight[i - 1] = true;
+          setDataIndex(i - 1);
           setIsHighlight([...isHighlight]);
         }
         break;
       }
-      console.log(typeof(isHighlight));
+      console.log(typeof isHighlight);
     }
-  }
-  const rightTesti = () =>{
-    for(let i=0;i<isHighlight.length;i++){
-      if(isHighlight[i]===true)
-      {
-        if(i===isHighlight.length-1)
-        {
+  };
+  const rightTesti = () => {
+    for (let i = 0; i < isHighlight.length; i++) {
+      if (isHighlight[i] === true) {
+        if (i === isHighlight.length - 1) {
           setDataIndex(0);
-          setIsHighlight([true,false,false,false]);
-        }
-        else{
+          setIsHighlight([true, false, false, false]);
+        } else {
           isHighlight[i] = false;
-          isHighlight[i+1] = true;
-          setDataIndex(i+1);
+          isHighlight[i + 1] = true;
+          setDataIndex(i + 1);
           setIsHighlight([...isHighlight]);
         }
         break;
       }
     }
-  }
+  };
+  AOS.init();
   return (
     <div className="Testimonial">
-      <Card name={names} company={companies} feedback={feedbacks} index={dataIndex} />
+      <Card
+        name={names}
+        company={companies}
+        feedback={feedbacks}
+        index={dataIndex}
+      />
       <div className="bottom_testi">
         <button className="left_slide">
           <BsArrowLeft onClick={leftTesti} />
         </button>
         <div className="testi_dots">
-          <div className={isHighlight[0] ? 'dot highlight' : 'dot'}></div>
-          <div className={isHighlight[1] ? 'dot highlight' : 'dot'}></div>
-          <div className={isHighlight[2] ? 'dot highlight' : 'dot'}></div>
-          <div className={isHighlight[3] ? 'dot highlight' : 'dot'}></div>
+          <div className={isHighlight[0] ? "dot highlight" : "dot"}></div>
+          <div className={isHighlight[1] ? "dot highlight" : "dot"}></div>
+          <div className={isHighlight[2] ? "dot highlight" : "dot"}></div>
+          <div className={isHighlight[3] ? "dot highlight" : "dot"}></div>
         </div>
         <button className="right_slide">
           {" "}
--- src/components/testimonial/testimonial_card.jsx
+++ src/components/testimonial/testimonial_card.jsx
@@ -3,7 +3,11 @@ import "./testimonial.css";
 import Customer from "../../assets/testimonial.png";
 import Vector1 from "../../assets/grey_testi.png";
 import Vector2 from "../../assets/blue_testi.png";
+import "aos/dist/aos.css";
+import AOS from "aos";
 const testimonial_card = (props) => {
+
+  AOS.init();
   return (
     <div className="testimonial_container">
       <img src={Vector1} alt="" className="testi_vector1" />
@@ -11,7 +15,57 @@ const testimonial_card = (props) => {
         <h2 className="testi_heading">What our customer are saying</h2>
         <div className="testi_underline"></div>
       </div>
-      <div className="testi_container_bottom">
+      <div className="testi_container_bottom"
+      style={{display: props.index === 0 ? 'flex' : 'none'}}
+      >
+        <div className="testi_left">
+          <img src={Customer} alt="" />
+          <div className="customer_info">
+            <small className="customer_name">{props.name[props.index]}</small>
+            <small className="customer_company">{props.company[props.index]}</small>
+          </div>
+        </div>
+        <div className="testi_right">
+          <p className="customer_review">
+            {props.feedback[props.index]}
+          </p>
+        </div>
+      </div>
+      <div className="testi_container_bottom"
+      style={{display:  props.index === 1 ? 'flex' : 'none'}}
+      >
+        <div className="testi_left">
+          <img src={Customer} alt="" />
+          <div className="customer_info">
+            <small className="customer_name">{props.name[props.index]}</small>
+            <small className="customer_company">{props.company[props.index]}</small>
+          </div>
+        </div>
+        <div className="testi_right">
+          <p className="customer_review">
+            {props.feedback[props.index]}
+          </p>
+        </div>
+      </div>
+      <div className="testi_container_bottom"
+      style={{display: props.index === 2 ? 'flex' :  'none'}}
+      >
+        <div className="testi_left">
+          <img src={Customer} alt="" />
+          <div className="customer_info">
+            <small className="customer_name">{props.name[props.index]}</small>
+            <small className="customer_company">{props.company[props.index]}</small>
+          </div>
+        </div>
+        <div className="testi_right">
+          <p className="customer_review">
+            {props.feedback[props.index]}
+          </p>
+        </div>
+      </div>
+      <div className="testi_container_bottom"
+      style={{display: props.index === 3 ? 'flex' :  'none'}}
+      >
         <div className="testi_left">
           <img src={Customer} alt="" />
           <div className="customer_info">
