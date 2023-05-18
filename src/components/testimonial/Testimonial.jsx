7c061570387395ab169b637a9fc47d34d9983cdb
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-05-18 23:48:46 +0530

    :fire: completed the logic behind changing the dot highlight in the bottom of the testomonial

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,25 +1,61 @@
-import React from "react";
+import {React, useState} from "react";
 import "./testimonial.css";
 import Card from "./testimonial_card";
 import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
 
 const Testimonial = () => {
+  const [isHighlight,setIsHighlight] = useState([false,true,false,false]);
+  const leftTesti = () =>{
+    for(let i=0;i<isHighlight.length;i++){
+      if(isHighlight[i]===true)
+      {
+        if(i===0)
+        {
+          setIsHighlight([false,false,false,true]);
+        }
+        else{
+          isHighlight[i] = false;
+          isHighlight[i-1] = true;
+          setIsHighlight([...isHighlight]);
+        }
+        break;
+      }
+      console.log(typeof(isHighlight));
+    }
+  }
+  const rightTesti = () =>{
+    for(let i=0;i<isHighlight.length;i++){
+      if(isHighlight[i]===true)
+      {
+        if(i===isHighlight.length-1)
+        {
+          setIsHighlight([true,false,false,false]);
+        }
+        else{
+          isHighlight[i] = false;
+          isHighlight[i+1] = true;
+          setIsHighlight([...isHighlight]);
+        }
+        break;
+      }
+    }
+  }
   return (
     <div className="Testimonial">
       <Card />
       <div className="bottom_testi">
         <button className="left_slide">
-          <BsArrowLeft />
+          <BsArrowLeft onClick={leftTesti} />
         </button>
         <div className="testi_dots">
-          <div className="dot highlight"></div>
-          <div className="dot"></div>
-          <div className="dot"></div>
-          <div className="dot"></div>
+          <div className={isHighlight[0] ? 'dot highlight' : 'dot'}></div>
+          <div className={isHighlight[1] ? 'dot highlight' : 'dot'}></div>
+          <div className={isHighlight[2] ? 'dot highlight' : 'dot'}></div>
+          <div className={isHighlight[3] ? 'dot highlight' : 'dot'}></div>
         </div>
         <button className="right_slide">
           {" "}
-          <BsArrowRight />
+          <BsArrowRight onClick={rightTesti} />
         </button>
       </div>
     </div>
